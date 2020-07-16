8b6896490e699e01f4fb44d5e19867227ba7c224
ckdesir <christopher.desir01@gmail.com>
2020-07-16 12:33:42 -0400

    Adds beginning of servlet

--- src/main/webapp/scripts/sessioncache.js
+++ src/main/webapp/scripts/sessioncache.js
@@ -1,5 +1,5 @@
-import { Poller } from "./poller";
-import { Session } from "./session";
+import { Poller } from './poller';
+import { Session } from './session';
 
 /** 
  * SessionCache bridges the gap between the client and server.
@@ -8,8 +8,7 @@ import { Session } from "./session";
  * 
  * SessionCache looks for information about the current session and
  * updates the server with information about each client in return (like
- * date last contacted). Also includes some additional functionality, 
- * like contacting the server to update the controller of the session.
+ * date last contacted).
  */
 class SessionCache {
   /**
@@ -18,20 +17,17 @@ class SessionCache {
    *    session the client is in, holds information such as the
    *    session ID and the screen name of the current user.
    * @param {number=} [refreshCadence = 30000] Represents the cadence at
-   *    which the sessionInformation is refreshed. By default, the rate is 
+   *    which the sessionInformation is refreshed. By default, the rate is
    *    30,000 milliseconds (or 30 seconds).  
-   * @param {number=} [pollingCadence = 30000] Represents the 
-   *    cadence at which the server is polled. By default, the rate is 
-   *    30,000 milliseconds (or 30 seconds). 
    */
-  constructor(urlParams, refreshCadence = 30000, pollingCadence = 30000) {
+  constructor(urlParams, refreshCadence = 30000) {
     /** 
      * Poller responsible for contacting the server for information about
      * the current session.
      * @private {Object} 
      */
     this.sessionInformationPoller_ = 
-        new Poller(this.sessionInformationRequest_, pollingCadence);
+        new Poller(this.sessionInformationRequest_, refreshCadence);
 
     /**
      * Holds what is being tracked by the SessionCache, the
@@ -41,65 +37,37 @@ class SessionCache {
     this.sessionInformation_ = null;
 
     /**
-     * @private {number}
-     */
-    this.refreshCadence_ = refreshCadence;
-
-    /**
      * @private {Object}
      */
     this.urlParams_ = urlParams;
   }
 
-/**
-   * Refreshes (rate dictated by the refreshCadence) 
-   * the result from the session information poller 
-   * and updates the sessionInformation field.
-   * @private
-   */
-  async refreshSessionInformation_() {
-    this.sessionInformation_ = 
-        await this.sessionInformationPoller_.getLastResult().then(
-            result => {
-              return result;
-            });
-    /**
-     * Represents the handler returned by the setTimeout that refreshes.
-     * @private {number}
-     */
-    this.setTimeoutIdOfRefresh_ = setTimeout(() => {
-      this.refreshSessionInformation_();
-    }, this.refreshCadence_);
-  }
-
   /** 
-   * This method begins polling for session information and starts
-   * refreshing.
+   * This method begins polling for session information.
    */
   start() {
     this.sessionInformationPoller_.start();
-    this.refreshSessionInformation_();
   }
 
   /** 
-   * This method stops polling for session information and stops
-   * refreshing.
+   * This method stops polling for session information.
    */
   stop() {
     this.sessionInformationPoller_.stop();
-    clearTimeout(this.setTimeoutIdOfRefresh_);
   }
 
   /**
    * Method sessionInformationRequest_() is the fetch api request
-   * responsible for gathering information about the current session 
-   * the client is in.
+   * responsible for gathering information about the current session.
    * @private
    */
   async sessionInformationRequest_() {
-    const response = await fetch('/get-session-info');
-    const sessionInfo = await response.json();
-    return sessionInfo;
+    const /** string */ name = encodeURI(this.urlParams_.get('name'));
+    const /** string */ sessionID = 
+        encodeURI(this.urlParams_.get('session-id'));
+    const /** Object */ response = await fetch(
+        `/get-session-info?name=${name}&session-id=${sessionID}`);
+    return await response.json();
   }
 
   /**
@@ -107,8 +75,10 @@ class SessionCache {
    * cache is in refreshing.
    * @return {Object} The Session object.
    */
-  getSessionInformation() {
-    return new Session(this.sessionInformation_);
+  async getSessionInformation() {
+    this.sessionInformation_ = 
+        await this.sessionInformationPoller_.getLastResult();
+    return Session.fromObject(this.sessionInformation_);
   }
 }
 
