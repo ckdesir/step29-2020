809168cfc017132f96cd7edd9539be62f655b45f
ckdesir <christopher.desir01@gmail.com>
2020-07-20 16:56:03 -0400

    n

--- src/main/webapp/scripts/sessioncache.test.js
+++ src/main/webapp/scripts/sessioncache.test.js
@@ -44,15 +44,13 @@ test('Test to see if stop is working correctly!', (done) => {
 test('We can check if poll() is called correct amount' + 
     'of times + correct return value', (done) => {
       fetch.mockResponse(JSON.stringify(expected));
-
       const cache = new SessionCache(params, 1000, 1000);
       cache.start();
-
-      setTimeout(() => {
+      setTimeout(async () => {
         expect(clearTimeoutSpy).toHaveBeenCalledTimes(0);
         expect(setTimeoutSpy.mock.calls.length).toBeGreaterThan(10);
         expect(pollSpy.mock.calls.length).toBeGreaterThanOrEqual(5);
-        expect(cache.getSessionInformation()).toEqual(expected);
+        await expect(cache.getSessionInformation()).resolves.toEqual(expected);
         done();
       }, 5000);
 });
@@ -64,15 +62,9 @@ test.only('stopping before starting', async () => {
   expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
   expect(pollSpy).toHaveBeenCalledTimes(0);
   expect(setTimeoutSpy).toHaveBeenCalledTimes(0);
-  try {
-    await expect(cache.getSessionInformation()).resolves.toEqual(expected);
-  } catch(e) {
-    expect(e.message).toBe('No contact with server.');
-  }
   cache.getSessionInformation().catch(error => {
     expect(error.message).toBe('No contact with server.');
   });
-  //expect(cache.getSessionInformation()).toBeFalsy();
 });
 
 test.only('starting up, immediately stopping', async () => {
@@ -83,22 +75,20 @@ test.only('starting up, immediately stopping', async () => {
   expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
   expect(pollSpy).toHaveBeenCalledTimes(1);
   expect(setTimeoutSpy).toHaveBeenCalledTimes(1);
-  cache.getSessionInformation().then(result => {
-    expect(result).toEqual(expected);
-  });
+  await expect(cache.getSessionInformation()).resolves.toEqual(expected);
 });
 
-test('starting up after stopping', (done) => {
+test.only('starting up after stopping', (done) => {
   fetch.mockResponse(JSON.stringify(expected));
   const cache = new SessionCache(params, 1000, 1000);
   cache.start();
   cache.stop();
   cache.start();
-  setTimeout(() => {
-    expect(clearTimeoutSpy).toHaveBeenCalledTimes(2);
-    expect(setTimeoutSpy.mock.calls.length).toBeGreaterThanOrEqual(12);
-    expect(pollSpy.mock.calls.length).toBeGreaterThanOrEqual(10);
-    expect(cache.getSessionInformation()).toEqual(expected);   
+  setTimeout(async () => {
+    expect(clearTimeoutSpy).toHaveBeenCalledTimes(1);
+    expect(setTimeoutSpy.mock.calls.length).toBeGreaterThanOrEqual(6);
+    expect(pollSpy.mock.calls.length).toBeGreaterThanOrEqual(6);
+    await expect(cache.getSessionInformation()).resolves.toEqual(expected);
     done();
   }, 5000);
 });
