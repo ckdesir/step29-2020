ea3a51ac1de366a01b87f176298c51b5a18b5dde
ckdesir <christopher.desir01@gmail.com>
2020-07-22 13:40:05 -0400

    n

--- src/main/java/com/google/sps/servlets/GetSessionServlet.java
+++ src/main/java/com/google/sps/servlets/GetSessionServlet.java
@@ -1,6 +1,11 @@
 package com.google.sps.servlets;
 
+import com.google.gson.*;
+
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +17,17 @@ public class GetSessionServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    throw new Error("Unimplemented");
+    String sessionId =
+        URLDecoder.decode(request.getParameter("session-id"), StandardCharsets.UTF_8);
+    String name = URLDecoder.decode(request.getParameter("name"), StandardCharsets.UTF_8);
+    // Session session = retrieveSession(sessionId);
+    // updateDateLastPolled(sessionId, name);
+    // List<String/Attendee?> listOfAttendees = getListOfAttendees(sessionId);
+    Gson gson = new Gson();
+    JsonElement jsonElement = gson.toJsonTree(session);
+    jsonElement.getAsJsonObject().addProperty("listOfAttendees", String.join(", ", listOfAttendees);
+    String json = gson.toJson(jsonElement);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
