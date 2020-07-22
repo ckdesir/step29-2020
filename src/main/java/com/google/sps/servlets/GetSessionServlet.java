fe774a09cf9f6d591bd6b53adca722067237baf4
ckdesir <christopher.desir01@gmail.com>
2020-07-22 18:56:35 -0400

    Adds everything.

--- src/main/java/com/google/sps/servlets/GetSessionServlet.java
+++ src/main/java/com/google/sps/servlets/GetSessionServlet.java
@@ -1,15 +1,11 @@
 package com.google.sps.servlets;
 
+import com.google.sps.data.DatastoreManager;
+import com.google.sps.data.DatastoreManagerInterface;
 import com.google.gson.*;
-
-import org.json.simple.*;
-
-import java.util.*;
-
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,17 +17,18 @@ public class GetSessionServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    DatastoreManagerInterface datastoreManager = new DatastoreManager();
     String sessionId =
         URLDecoder.decode(request.getParameter("session-id"), StandardCharsets.UTF_8);
     String name = URLDecoder.decode(request.getParameter("name"), StandardCharsets.UTF_8);
-    // Session session = retrieveSession(sessionId);
-    // updateDateLastPolled(sessionId, name);
-    // List<String/Attendee?> listOfAttendees = getListOfAttendees(sessionId);
+    Session session = datastoreManager.getSession(sessionId);
+    datastoreManager.updateAttendee(name, sessionId);
+    List<String> listOfAttendees = datastoreManager.getListOfAttendeesInSession(sessionId);
     Gson gson = new Gson();
     JsonElement jsonElement = gson.toJsonTree(session);
     jsonElement.getAsJsonObject().addProperty("listOfAttendees", gson.toJson(listOfAttendees));
-    //String json = gson.toJson(jsonElement);
+    String json = gson.toJson(jsonElement);
     response.setContentType("application/json;");
-    response.getWriter().println(gson.toJson(jsonElement));
+    response.getWriter().println(json);
   }
 }
