cfe9e7716412e6273da421cbd0b636016840935c
ckdesir <christopher.desir01@gmail.com>
2020-07-26 13:50:51 -0400

    n

--- src/main/webapp/scripts/session-script.js
+++ src/main/webapp/scripts/session-script.js
@@ -1,16 +1,184 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * Represents (in miliseconds) the cadence at which the UI is updated
+ * (if needed). 
+ * @type {number}
+ */
+const UPDATE_UI_CADENCE_MS = 30000;
+
+/**
+ * Represents (in miliseconds) how long the message that alerts users
+ * of any membership changes in the session is displayed. 
+ * @type {number}
+ */
+const MESSAGE_DURATION_MS = 4000;
+
+/**
+ * An array of who is currently in the session.
+ * @type {Array}
+ */
+let currentAttendees = [];
+
+/**
+ * Represents the noVNC client object; the single connection to the 
+ * VNC server.
+ * @type {RFB}
+ */
+let sessionScreen;
+
+/**
+ * Represents the URLSearchParams of the
+ * the client is in, holds information such as the
+ * session ID and the screen name of the current user.
+ * @type {URLSearchParams}
+ */
+const urlParameters = new URLSearchParams(window.location.search);
+
+/**
+ * Represents the ServerClient object responsible for
+ * keeping up-to-date with the current session and handles many
+ * of the client-to-server interactions, like passing the controller.
+ * @type {ServerClient}
+ */
+const client = new ServerClient(urlParameters);
+
+/**
+ * function updateUI() refreshes information client side, 
+ * updating the UI in checking for new attendees and for
+ * whoever the controller is.
+ */
+function updateUI() {
+  setInterval(() => {
+    client.getSession().then(session => {
+      updateSessionInfoAttendees(session.getListOfAttendees(),
+          session.getScreenNameOfController());
+      updateController(session.getScreenNameOfController());
+    });
+  }, UPDATE_UI_CADENCE_MS);
+}
+
+/**
+ * function updateSessionInfoAttendees() adds new attendees to the
+ * session to the session info attendee div. Also removes attendees 
+ * if they left the session. Alerts users of anyone who has left/entered.
+ * @param {Array} updatedAttendees array of new attendees
+ * @param {string} controller
+ */
+function updateSessionInfoAttendees(updatedAttendees, controller) {
+  const /** Array */ newAttendees = updatedAttendees.filter(attendee => {
+    return !currentAttendees.includes(attendee);
+  });
+  const /** Array */ attendeesThatHaveLeft = 
+      currentAttendees.filter(attendee => {
+        return !updatedAttendees.includes(attendee);
+  });
+  if (newAttendees.length > 0) {
+    let /** string */ displayMessage =
+        'The following people have joined the session: ';
+    displayMessage += newAttendees.join(', ');
+    if (attendeesThatHaveLeft.length > 0) {
+      displayMessage = 
+          displayMessage.substring(0, displayMessage.length-1) + 
+              '. The following people have left the session: ';
+      displayMessage += attendeesThatHaveLeft.join(', ');
+    }
+    notifyOfChangesToMembership(displayMessage);
+  } else if (newAttendees.length === 0 && attendeesThatHaveLeft.length 
+        > 0) {
+          let /** string */ displayMessage = 
+              'The following people have left the session: ';
+          displayMessage += attendeesThatHaveLeft.join(', ');
+          notifyOfChangesToMembership(displayMessage);
+        }
+  currentAttendees = updatedAttendees;
+  const /** HTMLElement */ sessionInfoAttendeesDiv =
+      document.getElementById('session-info-attendees');
+  sessionInfoAttendeesDiv.innerHTML = '';
+  currentAttendees.forEach(name => {
+    buildAttendeeDiv(name, controller);
+  });
+}
+
+/**
+ * function notifyOfChangesToMembership() notifies 
+ * users the message that's passed in.
+ * @param {string} displayMessage message to display to users
+ */
+function notifyOfChangesToMembership(displayMessage) {
+  console.log(displayMessage);
+  displayMessage = `${displayMessage.
+      substring(0, displayMessage.length-1)}.`;
+  const alertMembershipDiv = document.getElementById('alert-membership');
+  alertMembershipDiv.textContent = displayMessage;
+  alertMembershipDiv.className = 'display-message';
+  setTimeout(() => { 
+    alertMembershipDiv.className = ''; 
+  }, MESSAGE_DURATION_MS);
+}
+
+/**
+ * function buildAttendeeDiv() adds the div element containing
+ * all the elements representing an attendee to the session info
+ * attendees div.
+ * @param {string} nameOfAttendee name of attendee to build
+ * @param {string} controller
+ */
+function buildAttendeeDiv(nameOfAttendee, controller) {
+  const /** HTMLElement */ sessionInfoAttendeesDiv =
+      document.getElementById('session-info-attendees');
+  const /** HTMLDivElement */ attendeeDiv = document.createElement('div');
+  attendeeDiv.className = 'attendee-div'
+  const /** HTMLSpanElement */ controllerToggle = 
+      document.createElement('span');
+  controllerToggle.className = 'controller-toggle';
+  controllerToggle.addEventListener('click', event => {
+    passController(event, controller);
+  }, /**AddEventListenerOptions=*/false);
+  const /** HTMLHeadingElement */ attendeeName =
+      document.createElement('h3');
+  attendeeName.innerHTML = nameOfAttendee;
+  attendeeName.className = 'attendee-name'
+  attendeeName.id = nameOfAttendee;
+  attendeeDiv.appendChild(controllerToggle);
+  attendeeDiv.appendChild(attendeeName);
+  sessionInfoAttendeesDiv.appendChild(attendeeDiv);
+}
+
+/**
+ * If the current controller of the session clicks on the controller 
+ * toggle, their controller status is revoked and the server is updated
+ * with information on the new controller.
+ * @param {MouseEvent} event
+ * @param {string} controller
+ */
+function passController(event, controller) {
+  if (urlParameters.get('name') === controller) {
+    sessionScreen.viewOnly = true;
+    client.passController(
+        event.target.parentElement.querySelector('h3').id);
+  }
+}
+
+/**
+ * function updateController() checks to see if the current user should
+ * be the controller of their party, changing session screen privilege
+ * and updating user interface.
+ * @param {string} controller
+ */
+function updateController(controller) {
+  const /** HTMLElement */ sessionInfoAttendeesDiv =
+      document.getElementById('session-info-attendees');
+  const /** NodeListOf<HTMLSpanElement> */ controllerToggleList = 
+      sessionInfoAttendeesDiv.querySelectorAll('span');
+  if (urlParameters.get('name') === controller) {
+    sessionScreen.viewOnly = false;
+  }
+  controllerToggleList.forEach(individualSpanElement => {
+    individualSpanElement.style.backgroundColor = '#fff';
+  });
+  sessionInfoAttendeesDiv.querySelector(`#${controller}`)
+          .parentElement.querySelector('span').style.
+              backgroundColor = '#fd5d00';
+}
 
 /**
  * function openSessionInfo() displays the div container
@@ -39,8 +207,6 @@ function copyTextToClipboard() {
   document.execCommand('copy');
 }
 
-module.exports = {
-  openSessionInfo: openSessionInfo,
-  closeSessionInfo: closeSessionInfo,
-  copyTextToClipboard: copyTextToClipboard
-};
+export { openSessionInfo, copyTextToClipboard,  
+  updateController, passController,
+  buildAttendeeDiv, updateSessionInfoAttendees };
--- src/main/webapp/scripts/session-script.test.js
+++ src/main/webapp/scripts/session-script.test.js
@@ -1,5 +1,16 @@
-const {openSessionInfo, closeSessionInfo, copyTextToClipboard} =
-    require('./session-script');
+import * as sessionscript from './session-script';
+    require('./session-script');	import { Session } from './Session';
+import { ServerClient } from './serverclient';
+
+const buildAttendeeDivSpy =
+    jest.spyOn(sessionscript.sessionScriptSpies, 'buildAttendeeDiv');
+const notifyOfChangesToMembershipSpy =
+    jest.spyOn(sessionscript.sessionScriptSpies,
+        'notifyOfChangesToMembership');
+
+afterEach(() => {    
+  jest.clearAllMocks();
+});
 
 test('display none to block', () => {
   document.body.innerHTML = '<div id="container"></div>';
@@ -73,4 +84,207 @@ test('tests copy and paste', () => {
   input.click();
 
   expect(document.execCommand).toHaveBeenCalledWith('copy');
+});
+
+test('adding an attendee div', () => {
+  document.body.innerHTML = '';
+  const sessionInfoAttendeeDiv =
+      document.createElement('div');
+  sessionInfoAttendeeDiv.id = 'session-info-attendees';
+  document.body.appendChild(sessionInfoAttendeeDiv);
+  const attendeeDivExpected = document.createElement('div');
+  attendeeDivExpected.className = 'attendee-div'
+  const controllerToggle = 
+      document.createElement('span');
+  controllerToggle.className = 'controller-toggle';
+  controllerToggle.addEventListener('click', event => {
+    sessionscript.changeController(event, 'Bryan');
+  }, false);  
+  const attendeeName = document.createElement('h3');
+  attendeeName.innerHTML = 'hello';
+  attendeeName.className = 'attendee-name'
+  attendeeName.id = 'hello';
+  attendeeDivExpected.appendChild(controllerToggle);
+  attendeeDivExpected.appendChild(attendeeName);
+  sessionscript.buildAttendeeDiv('hello', 'Bryan');
+  expect(sessionInfoAttendeeDiv.childNodes[0]).
+      toEqual(attendeeDivExpected);
+});
+
+test('Tests to see if controller updates correctly UI wise', () => {
+  document.body.innerHTML = '';
+  const sessionInfoAttendeeDiv =
+      document.createElement('div');
+  sessionInfoAttendeeDiv.id = 'session-info-attendees';
+  document.body.appendChild(sessionInfoAttendeeDiv);
+  sessionscript.buildAttendeeDiv('Jessica', 'Jessica');
+  sessionscript.buildAttendeeDiv('Bryan', 'Jessica');
+  sessionscript.buildAttendeeDiv('Chris', 'Jessica');
+  const urlParamSpy = 
+      jest.spyOn(window.URLSearchParams.prototype, 'get').
+          mockReturnValue('Jessica');
+  const sessionSpy = 
+      jest.spyOn(Session.prototype, 'getScreenNameOfController').
+          mockReturnValue('Jessica');
+  sessionscript.updateController('Jessica');
+  expect(sessionInfoAttendeeDiv.querySelector(`#${'Jessica'}`)
+      .parentElement.querySelector('span').style.
+          backgroundColor).toEqual('rgb(253, 93, 0)');
+  expect(sessionInfoAttendeeDiv.querySelector(`#${'Bryan'}`)
+      .parentElement.querySelector('span').style.
+          backgroundColor).toEqual('rgb(255, 255, 255)');
+  expect(sessionInfoAttendeeDiv.querySelector(`#${'Chris'}`)
+      .parentElement.querySelector('span').style.
+          backgroundColor).toEqual('rgb(255, 255, 255)');
+});
+
+test('tests passController() - controller clicks', () => {
+  const urlParamSpy = 
+      jest.spyOn(window.URLSearchParams.prototype, 'get').
+          mockReturnValue('Jessica');
+  const passControllerSpy = 
+      jest.spyOn(ServerClient.prototype, 'passController');
+  const attendeeDiv = document.createElement('div');
+  attendeeDiv.className = 'attendee-div'
+  const controllerToggle = 
+      document.createElement('span');
+  controllerToggle.className = 'controller-toggle';
+  controllerToggle.addEventListener('click', event => {
+    sessionscript.passController(event, 'Jessica');
+  }, false);
+  const attendeeName = document.createElement('h3');
+  attendeeName.innerHTML = 'Naomi';
+  attendeeName.className = 'attendee-name';
+  attendeeName.id = 'Naomi';
+  attendeeDiv.appendChild(controllerToggle);
+  attendeeDiv.appendChild(attendeeName);
+  controllerToggle.click();
+  expect(passControllerSpy).toBeCalledWith('Naomi');
+});
+
+test('tests passController() - controller does not click', () => {
+  const urlParamSpy = 
+      jest.spyOn(window.URLSearchParams.prototype, 'get').
+          mockReturnValue('Jessica');
+  const passControllerSpy = 
+      jest.spyOn(ServerClient.prototype, 'passController');
+  const attendeeDiv = document.createElement('div');
+  attendeeDiv.className = 'attendee-div'
+  const controllerToggle = 
+      document.createElement('span');
+  controllerToggle.className = 'controller-toggle';
+  controllerToggle.addEventListener('click', 
+      sessionscript.passController, false);
+  const attendeeName =
+      document.createElement('h3');
+  attendeeName.innerHTML = 'Bob';
+  attendeeName.className = 'attendee-name'
+  attendeeName.id = 'Bob';
+  attendeeDiv.appendChild(controllerToggle);
+  attendeeDiv.appendChild(attendeeName);
+  controllerToggle.click();
+  expect(passControllerSpy).toBeCalledTimes(0);
+});
+
+test(`makes sure notifyOfChangesToMembership is
+correctly displaying message`, (done) => {
+    const displayMessage = 'How are you ';
+    document.body.innerHTML = '';
+    const alertMembershipDiv =
+        document.createElement('div');
+    alertMembershipDiv.id = 'alert-membership';
+    document.body.appendChild(alertMembershipDiv);
+    sessionscript.notifyOfChangesToMembership(displayMessage);
+    setTimeout(() => {
+      expect(alertMembershipDiv.textContent).toEqual('How are you.');
+      expect(alertMembershipDiv.className).toEqual('display-message');
+      done();
+    }, 2000);
+    setTimeout(() => {
+      expect(alertMembershipDiv.className).toEqual('');
+      done();
+    }, 6000);
+});
+
+test(`A new member 
+    -updateSessionInfoAttendees`, () => {
+      const expectedMessage =
+          `The following people have joined the session: ${'Miguel'} `;
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      const alertMembershipDiv =
+          document.createElement('div');
+      alertMembershipDiv.id = 'alert-membership';
+      document.body.appendChild(alertMembershipDiv);
+      sessionscript.updateSessionInfoAttendees(['Jessica', 'Bryan', 'Miguel'], 'Jessica');
+      expect(notifyOfChangesToMembershipSpy).
+          toHaveBeenCalledWith(expectedMessage);
+      expect(buildAttendeeDivSpy).toBeCalledTimes(3);
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Jessica', 'Jessica');
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Bryan', 'Jessica');
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Miguel', 'Jessica');
+});
+
+test(`A member that has left
+    -updateSessionInfoAttendees`, () => {
+      const expectedMessage =
+          `The following people have left the session: ${'Bryan'} `;
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      const alertMembershipDiv =
+          document.createElement('div');
+      alertMembershipDiv.id = 'alert-membership';
+      document.body.appendChild(alertMembershipDiv);
+      const sessionSpy = 
+          jest.spyOn(Session.prototype, 'getListOfAttendees').
+              mockReturnValue(['Jessica']);
+      sessionscript.updateSessionInfoAttendees(['Jessica'], 'Bryan');
+      expect(notifyOfChangesToMembershipSpy).
+         toHaveBeenCalledWith(expectedMessage);
+      expect(buildAttendeeDivSpy).toBeCalledTimes(1);
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Jessica', 'Bryan');
+
+    });
+
+test(`A new member + a lost member' + 
+    '-updateSessionInfoAttendees`, () => {
+      const expectedMessage =
+          'The following people have joined the session: ' + 'Miguel.' + 
+              ' The following people have left the session: ' + 'Bryan ';
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      const alertMembershipDiv =
+          document.createElement('div');
+      alertMembershipDiv.id = 'alert-membership';
+      document.body.appendChild(alertMembershipDiv);
+      const sessionSpy = 
+          jest.spyOn(Session.prototype, 'getListOfAttendees').
+              mockReturnValue(['Jessica', 'Miguel']);
+      sessionscript.updateSessionInfoAttendees(['Jessica', 'Miguel'], 'Jessica');
+      expect(notifyOfChangesToMembershipSpy).
+          toHaveBeenCalledWith(expectedMessage);
+      expect(buildAttendeeDivSpy).toBeCalledTimes(2);
+      expect(buildAttendeeDivSpy).toBeCalledWith('Jessica', 'Jessica');
+      expect(buildAttendeeDivSpy).toBeCalledWith('Miguel', 'Jessica');
+});
+
+test(`no update 
+    '-updateSessionInfoAttendees`, () => {
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      sessionscript.updateSessionInfoAttendees(['Jessica', 'Bryan'], 'Bryan');
+      expect(buildAttendeeDivSpy).toBeCalledWith('Jessica', 'Bryan');
+      expect(buildAttendeeDivSpy).toBeCalledWith('Bryan', 'Bryan');
 });
