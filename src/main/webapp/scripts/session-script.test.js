9335ef24b2320ee75657fce6c6018f0a937db26f
ckdesir <christopher.desir01@gmail.com>
2020-07-29 16:35:55 -0400

    Adds tests for the new functions

--- src/main/webapp/scripts/session-script.test.js
+++ src/main/webapp/scripts/session-script.test.js
@@ -1,5 +1,15 @@
-const {openSessionInfo, closeSessionInfo, copyTextToClipboard} =
-    require('./session-script');
+import * as sessionscript from './session-script';
+import { Session } from './Session';
+
+const buildAttendeeDivSpy =
+    jest.spyOn(sessionscript.sessionScriptSpies, 'buildAttendeeDiv');
+const notifyOfChangesToMembershipSpy =
+    jest.spyOn(sessionscript.sessionScriptSpies,
+        'notifyOfChangesToMembership');
+
+afterEach(() => {    
+  jest.clearAllMocks();
+});
 
 test('display none to block', () => {
   document.body.innerHTML = '<div id="container"></div>';
@@ -73,4 +83,107 @@ test('tests copy and paste', () => {
   input.click();
 
   expect(document.execCommand).toHaveBeenCalledWith('copy');
+});
+
+test(`makes sure notifyOfChangesToMembership is
+correctly displaying message`, (done) => {
+    const displayMessage = 'How are you ';
+    document.body.innerHTML = '';
+    const alertMembershipDiv =
+        document.createElement('div');
+    alertMembershipDiv.id = 'alert-membership';
+    document.body.appendChild(alertMembershipDiv);
+    sessionscript.notifyOfChangesToMembership(displayMessage);
+    setTimeout(() => {
+      expect(alertMembershipDiv.textContent).toEqual('How are you.');
+      expect(alertMembershipDiv.className).toEqual('display-message');
+      done();
+    }, 2000);
+    setTimeout(() => {
+      expect(alertMembershipDiv.className).toEqual('');
+      done();
+    }, 6000);
+});
+
+test(`A new member 
+    -updateSessionInfoAttendees`, () => {
+      const expectedMessage =
+          `The following people have joined the session: ${'Miguel'} `;
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      const alertMembershipDiv =
+          document.createElement('div');
+      alertMembershipDiv.id = 'alert-membership';
+      document.body.appendChild(alertMembershipDiv);
+      sessionscript.updateSessionInfoAttendees(['Jessica', 'Bryan', 'Miguel'], 'Jessica');
+      expect(notifyOfChangesToMembershipSpy).
+          toHaveBeenCalledWith(expectedMessage);
+      expect(buildAttendeeDivSpy).toBeCalledTimes(3);
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Jessica', 'Jessica');
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Bryan', 'Jessica');
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Miguel', 'Jessica');
+});
+
+test(`A member that has left
+    -updateSessionInfoAttendees`, () => {
+      const expectedMessage =
+          `The following people have left the session: ${'Bryan'} `;
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      const alertMembershipDiv =
+          document.createElement('div');
+      alertMembershipDiv.id = 'alert-membership';
+      document.body.appendChild(alertMembershipDiv);
+      const sessionSpy = 
+          jest.spyOn(Session.prototype, 'getListOfAttendees').
+              mockReturnValue(['Jessica']);
+      sessionscript.updateSessionInfoAttendees(['Jessica'], 'Bryan');
+      expect(notifyOfChangesToMembershipSpy).
+         toHaveBeenCalledWith(expectedMessage);
+      expect(buildAttendeeDivSpy).toBeCalledTimes(1);
+      expect(buildAttendeeDivSpy).toHaveBeenCalledWith('Jessica', 'Bryan');
+
+    });
+
+test(`A new member + a lost member' + 
+    '-updateSessionInfoAttendees`, () => {
+      const expectedMessage =
+          'The following people have joined the session: ' + 'Miguel.' + 
+              ' The following people have left the session: ' + 'Bryan ';
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      const alertMembershipDiv =
+          document.createElement('div');
+      alertMembershipDiv.id = 'alert-membership';
+      document.body.appendChild(alertMembershipDiv);
+      const sessionSpy = 
+          jest.spyOn(Session.prototype, 'getListOfAttendees').
+              mockReturnValue(['Jessica', 'Miguel']);
+      sessionscript.updateSessionInfoAttendees(['Jessica', 'Miguel'], 'Jessica');
+      expect(notifyOfChangesToMembershipSpy).
+          toHaveBeenCalledWith(expectedMessage);
+      expect(buildAttendeeDivSpy).toBeCalledTimes(2);
+      expect(buildAttendeeDivSpy).toBeCalledWith('Jessica', 'Jessica');
+      expect(buildAttendeeDivSpy).toBeCalledWith('Miguel', 'Jessica');
+});
+
+test(`no update 
+    '-updateSessionInfoAttendees`, () => {
+      document.body.innerHTML = '';
+      const sessionInfoAttendeeDiv =
+          document.createElement('div');
+      sessionInfoAttendeeDiv.id = 'session-info-attendees';
+      document.body.appendChild(sessionInfoAttendeeDiv);
+      sessionscript.updateSessionInfoAttendees(['Jessica', 'Bryan'], 'Bryan');
+      expect(buildAttendeeDivSpy).toBeCalledWith('Jessica', 'Bryan');
+      expect(buildAttendeeDivSpy).toBeCalledWith('Bryan', 'Bryan');
 });
